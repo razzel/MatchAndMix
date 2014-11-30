16e8f2307bf6d77db4410e787cc055ee4a89a51b
razzel <aka.zelmendoza@yahoo.com>
2014-11-30 14:06:58 +0800

    ok na yung BG yan.. may nka limutan ka lng na iload sa ResourcesManager. See this: loadMatchItScenePanel() { createBackground(); etc etc .. } kaya naging black yung BG nya

--- src/com/kokostudio/matchandmix/scene/game/panel/MatchItPanel.java
+++ src/com/kokostudio/matchandmix/scene/game/panel/MatchItPanel.java
@@ -9,62 +9,45 @@ import com.kokostudio.matchandmix.base.BaseScene;
 
 import com.kokostudio.matchandmix.manager.SceneManager.SceneType;
 
-public class MatchItPanel extends BaseScene{
+public class MatchItPanel extends BaseScene {
 	
 	
 	private HUD mainMenuHUD;
 
 	@Override
 	public void createScene() {
-		// TODO Auto-generated method stub
 		createBackground();
-		createEntities();
-		
+		createEntities();	
 	}
 
 	private void createEntities() {
-		// TODO Auto-generated method stub
-		camera.setHUD(mainMenuHUD);
-		
+		camera.setHUD(mainMenuHUD);	
 	}
 
 	private void createBackground() {
-		// TODO Auto-generated method stub
 		attachChild(new Sprite(400,240, resourcesManager.bgTextureRegion, vbom) {
 			@Override
 			protected void draw(GLState pGLState, Camera pCamera) {
 				super.draw(pGLState, pCamera);
 				pGLState.enableDither();
-			}
-			
+			}	
 		});
 	}
 	
 
 	@Override
 	public void onBackKeyPressed() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public SceneType getSceneType() {
-	
 		return SceneType.SCENE_MATCHITPANEL;
 	}
 
 	@Override
 	public void disposeScene() {
-		camera.setHUD(null);
-		camera.setCenter(400, 240);
-	//	pause.detachSelf();
-	//	pause.dispose();
-	//	back.detachSelf();
-	//	back.dispose();
+		
 	}
 	
-
 }
-
-
-
